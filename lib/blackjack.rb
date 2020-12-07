39bf501ed2ab4d647373bde907c558d5136e79ad
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-07 03:11:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,18 +5,22 @@ end
 
 def deal_card
   # code #deal_card here
+  rand(12)
 end
 
-def display_card_total
+def display_card_total card_total
   # code #display_card_total here
+  puts card_total
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
